f3920c8af0f06a6ffeff99d3e1d049e463f91cfc
Peter Svensson <peter@sparetimecoders.com>
2026-02-04 09:04:58 +0100

    fix(vars): correct spelling in role regex descriptions
    Correct spelling of "selectef" to "selected" in the descriptions for 
    admin and read-only IAM role regex variables in vars.tf. This change 
    improves clarity and correctness of the variable documentation.

--- vars.tf
+++ vars.tf
@@ -1,11 +1,11 @@
 variable "admin_role_regex" {
   type        = string
   default     = "AWSReservedSSO_AWSAdministratorAccess_.*"
-  description = "Regular expression used to match the admin IAM role, if multiple roles match the first one will be selectef"
+  description = "Regular expression used to match the admin IAM role, if multiple roles match the first one will be selected"
 }
 
 variable "view_role_regex" {
   type        = string
   default     = "AWSReservedSSO_AWSReadOnlyAccess_.*"
-  description = "Regular expression used to match the read only IAM role, if multiple roles match the first one will be selectef"
+  description = "Regular expression used to match the read only IAM role, if multiple roles match the first one will be selected"
 }
